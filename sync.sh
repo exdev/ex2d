dc333a9c826159cf4d05d09d63ecfeaafdfd7623
Johnny Wu <johnny.w@mechanist.co>
2013-08-09 16:58:07 +0800

    v2.0 doc submition

--- sync.sh
+++ sync.sh
@@ -1,5 +1,7 @@
-dest=~/exdev/ex2D_Runtime/ex2D/Core
-src=~/exdev/ex2D/ex2d_dev/Assets/ex2D/Core
+# copy Runtime
+
+src=~/exdev/ex2d/ex2d_dev/Assets/ex2D/Runtime
+dest=~/exdev/ex2d_doc/Runtime
 
 rm -rf $dest
 mkdir -p $dest
@@ -9,16 +11,3 @@ find . -type d -name '*' -exec mkdir -p $dest/{} \;
 find . -type f -iname "*.cs" -exec cp {} $dest/{} \;
 find . -type f -iname "*.shader" -exec cp {} $dest/{} \;
 cd $dest
-
-dest_gui=~/exdev/ex2D_Runtime/ex2D_GUI/Core
-src_gui=~/exdev/ex2D/ex2d_dev/Assets/ex2D_GUI/Core
-
-rm -rf $dest_gui
-mkdir -p $dest_gui
-
-cd $src_gui
-find . -type d -name '*' -exec mkdir -p $dest_gui/{} \;
-find . -type f -iname "*.cs" -exec cp {} $dest_gui/{} \;
-find . -type f -iname "*.shader" -exec cp {} $dest_gui/{} \;
-cd $dest_gui
-
